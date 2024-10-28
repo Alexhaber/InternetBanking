4f68d6d2b5193c8d23f0943e47d5b394de014c73
samuellinaress <samuellinares660@gmail.com>
2024-10-28 14:06:24 -0400

    Se agrego la entidad Transaction a la capa Domain

---
+++ InternetBanking.Core.Domain/Entities/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace InternetBanking.Core.Domain.Entities
+{
+	public class Transaction
+	{
+		public int Id { get; set; }
+        public int SourceProductId { get; set; }
+        public int DestinationProductId { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Made { get; set; } = DateTime.Now;
+    }
+}
