79926ad897c6b4640b6278f6fefdab1de6e9a9aa
samuellinaress <samuellinares660@gmail.com>
2024-11-04 10:25:59 -0400

    Codigo de PaymentService y PaymentRepository refactorizado

--- InternetBanking.Core.Application/Interfaces/Services/IPaymentService.cs
+++ InternetBanking.Core.Application/Interfaces/Services/IPaymentService.cs
@@ -6,7 +6,6 @@ namespace InternetBanking.Core.Application.Interfaces.Services
     public interface IPaymentService
     {
         Task<DepositResponse> MakeCashAdvance(CreditCard card, SavingAccount savingAccount, decimal deposit);
-
         Task<DepositResponse> InterAccountTransaction(SavingAccount sender, SavingAccount receiver, decimal deposit);
 
 	}
